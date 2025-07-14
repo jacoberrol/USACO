ebe9fda3c0c0820dd5234131c8d254f22e41e866
jacoberrol <jacob.errol@gmail.com>
2025-07-14 16:24:36 -0400

    added bucket list

---
+++ bronze/bucket-list/python/main.py
@@ -0,0 +1,17 @@
+
+with open("blist.in","r") as read:
+    n = int(read.readline())
+    cows = [list(map(int,read.readline().split())) for _ in range(n)]
+
+max_time = max(t for _, t, _ in cows)
+
+time = [0] * (max_time+1)
+
+for s, t, b in cows:
+    for i in range(s, t+1):
+        time[i] += b
+
+with open("blist.out","w") as out:
+    print(max(time),file=out)
+
+    
